63c6884f210bd77053f186b80fd9e48104c01c1a
Setyokun <piscaprasetyoaw@gmail.com>
2019-12-23 12:27:48 +0700

    add file in folder body validator

---
+++ bodyValidator/postLogin.js
@@ -0,0 +1,12 @@
+const bodyLoginSuccessful = {
+    "token": "QpwL5tke4Pnpja7X4"
+}
+
+const bodyLoginUnsuccessful = {
+    "error": "Missing password"
+}
+
+module.exports = {
+    bodyLoginSuccessful,
+    bodyLoginUnsuccessful,
+}
---
+++ bodyValidator/postRegister.js
@@ -0,0 +1,12 @@
+const bodyRegisterSuccessful = {
+    "id": 4,
+    "token": "QpwL5tke4Pnpja7X4"
+}
+
+const bodyRegisterUnsuccessful = {
+    "error": "Missing password"
+}
+
+module.exports = {
+    bodyRegisterSuccessful, bodyRegisterUnsuccessful,
+}
